da9bb3dfc561b4a69bd1d3ecdc3c31adb69360fd
zixiazhang <zixiazhang84@gmail.com>
2013-11-28 12:25:13 +0000

    Renaming method and parameter to make intentions clearer

--- WordCount/Interfaces/ITrieNode.cs
+++ WordCount/Interfaces/ITrieNode.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace WordCount.Interfaces
 {
     public interface ITrieNode
     {
         TrieNode AddCharacter(char keyCharacter);
-        void GetWordCount(Dictionary<string, int> wordCountDictionary);
+        void PopulateWordCountDictionary(ref Dictionary<string, int> wordCountDictionary);
         string ToString();
     }
 }
